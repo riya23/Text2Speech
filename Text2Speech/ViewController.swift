4d32192362910ad68a2cb9f06e0d176ebfd25301
Riya Ganguly <rigangul@in.ibm.com>
2017-06-19 15:49:15 +0530

    Initial Commit

---
+++ Text2Speech/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Text2Speech
+//
+//  Created by Riya Ganguly on 19/06/17.
+//  Copyright © 2017 Riya Ganguly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
