ec5e93654ed988a46b81c9d0becd3a48e2d279ce
Riya Ganguly <rigangul@in.ibm.com>
2017-06-20 11:25:49 +0530

    changes

---
+++ Text2Speech/S2TViewController.swift
@@ -0,0 +1,155 @@
+//
+//  S2TViewController.swift
+//  Text2Speech
+//
+//  Created by Riya Ganguly on 19/06/17.
+//  Copyright © 2017 Riya Ganguly. All rights reserved.
+//
+
+import UIKit
+import Speech
+
+class S2TViewController: UIViewController , SFSpeechRecognizerDelegate {
+
+    @IBOutlet weak var txtView: UITextView!
+    
+    
+    @IBOutlet weak var btnMircoPhone: UIButton!
+    
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale.init(identifier: "en-US"))
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionTask: SFSpeechRecognitionTask?
+    private let audioEngine = AVAudioEngine()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Speech2Text"
+        navigationController?.navigationBar.barStyle = UIBarStyle.black
+        navigationController?.navigationBar.tintColor = UIColor.white
+        // Do any additional setup after loading the view.
+        
+        txtView.layer.borderWidth = 1.0
+        
+        speechRecognizer?.delegate = self
+        self.btnMircoPhone.isEnabled = false
+        
+        SFSpeechRecognizer.requestAuthorization { (authStatus) in  //4
+            
+            var isButtonEnabled = false
+            
+            switch authStatus {  //5
+            case .authorized:
+                isButtonEnabled = true
+                
+            case .denied:
+                isButtonEnabled = false
+                print("User denied access to speech recognition")
+                
+            case .restricted:
+                isButtonEnabled = false
+                print("Speech recognition restricted on this device")
+                
+            case .notDetermined:
+                isButtonEnabled = false
+                print("Speech recognition not yet authorized")
+            }
+            
+            OperationQueue.main.addOperation() {
+                self.btnMircoPhone.isEnabled = isButtonEnabled
+            }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func btnMicrophoneTapped(_ sender: UIButton) {
+        print("tap")
+        if audioEngine.isRunning {
+            audioEngine.stop()
+            recognitionRequest?.endAudio()
+            btnMircoPhone.isEnabled = false
+            btnMircoPhone.setTitle("Start Recording", for: .normal)
+        } else {
+            startRecording()
+            btnMircoPhone.setTitle("Stop Recording", for: .normal)
+        }
+    }
+    
+    func startRecording() {
+        
+        if recognitionTask != nil {
+            recognitionTask?.cancel()
+            recognitionTask = nil
+        }
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(AVAudioSessionCategoryRecord)
+            try audioSession.setMode(AVAudioSessionModeMeasurement)
+            try audioSession.setActive(true, with: .notifyOthersOnDeactivation)
+        } catch {
+            print("audioSession properties weren't set because of an error.")
+        }
+        
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        
+        guard let inputNode = audioEngine.inputNode else {
+            fatalError("Audio engine has no input node")
+        }
+        
+        guard let recognitionRequest = recognitionRequest else {
+            fatalError("Unable to create an SFSpeechAudioBufferRecognitionRequest object")
+        }
+        
+        recognitionRequest.shouldReportPartialResults = true
+        
+        recognitionTask = speechRecognizer?.recognitionTask(with: recognitionRequest, resultHandler: { (result, error) in
+            
+            var isFinal = false
+            
+            if result != nil {
+                self.txtView.text = ""
+                self.txtView.text = result?.bestTranscription.formattedString
+                isFinal = (result?.isFinal)!
+            }
+            
+            if error != nil || isFinal {
+                self.audioEngine.stop()
+                inputNode.removeTap(onBus: 0)
+                
+                self.recognitionRequest = nil
+                self.recognitionTask = nil
+                
+                self.btnMircoPhone.isEnabled = true
+            }
+        })
+        
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, when) in
+            self.recognitionRequest?.append(buffer)
+        }
+        
+        audioEngine.prepare()
+        
+        do {
+            try audioEngine.start()
+        } catch {
+            print("audioEngine couldn't start because of an error.")
+        }
+        
+        txtView.text = "Say something, I'm listening!"
+        
+    }
+    
+    func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+        if available {
+            btnMircoPhone.isEnabled = true
+        } else {
+            btnMircoPhone.isEnabled = false
+        }
+    }
+
+}
--- Text2Speech/ViewController.swift
+++ Text2Speech/ViewController.swift
@@ -7,19 +7,40 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtView: UITextView!
+    let synth = AVSpeechSynthesizer()
+    var myUtterance = AVSpeechUtterance(string: "")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Text2Speech"
+//        navigationController?.navigationBar.barTintColor = UIColor.black
+        navigationController?.navigationBar.barStyle = UIBarStyle.black
+        navigationController?.navigationBar.tintColor = UIColor.white
         // Do any additional setup after loading the view, typically from a nib.
+        txtView.layer.borderWidth = 1.0
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        txtView.text = "Tell Something!!"
+        
+    }
 
-
+    @IBAction func btnSpeechTapped(_ sender: UIButton) {
+        myUtterance = AVSpeechUtterance(string: txtView.text)
+        myUtterance.rate = AVSpeechUtteranceDefaultSpeechRate//0.5
+        myUtterance.voice = AVSpeechSynthesisVoice(language: "en-gb")
+        synth.speak(myUtterance)
+    }
 }
 
